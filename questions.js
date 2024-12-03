19b112f488c0ca5ced688e232d98878916bcb5b6
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-03 13:26:44 +0000

    Write 5 questions and start object questionOne
    Added in question_text as well.

--- questions.js
+++ questions.js
@@ -1,7 +1,34 @@
 // questions.js
+// question 1 What was the first pneumonic that we learnt week 1?
+// DRY, PIE, KISS, CHRIS 
+// question 2 Which one of these is a stage in computational thinking?
+// Panic, Use ChatGBT , Start coding straight way, Understand the problem 
+//question 3 What is in a high performance routine?
+// Preparation, Intense working, Wind-Up, No sleep
+// question 4 Which of these is not a stage in Disney ideation?
+// Dreamer, Optimist, Realist, Critic 
+// question 5 What was the same of the AI we hacked?
+// Dumbledore, Gandalf, Merlin, Morgana
+// question 6 
+
+// Designing object 
+// question_text: This is a string that contains the text of the question you want to ask.
+// question_type: This should always be the string "multiple_choice" for this task.
+// options: This is an array of strings, where each string is a possible answer to the question. You should provide four options.
+// correct_option: This is a number that represents the index of the correct answer in the options array. The index is 0-based, meaning the first option is 0, the second is 1, and so on.
+// correct_explanation: This is a string that explains why the correct answer is correct. This helps learners understand the reasoning behind the answer.
+
+let questionOne = {
+    question_text: "What was the first pneumonic that we learnt week 1?",
+    
+
+
+}
+
 
 const questions = [
   // Add your questions here
+
 ];
 
 // Don't worry about this, we're just exporting the questions
